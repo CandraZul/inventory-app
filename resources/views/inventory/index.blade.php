9508a98c154c870b3f707d54b1b01f9c4c447235
Refa Hilyah <refahilyaa@student.uns.ac.id>
2025-12-29 14:30:05 +0700

    inventory: benerin

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -5,15 +5,19 @@
 <table border="1">
     <tr>
         <th>Nama</th>
+        <th>Merk</th>
         <th>Jumlah</th>
         <th>Kondisi</th>
+        <th>Lokasi</th>
     </tr>
 
     @foreach($items as $item)
         <tr>
             <td>{{ $item->nama_barang }}</td>
+            <td>{{ $item->merk }}</td>
             <td>{{ $item->jumlah }}</td>
             <td>{{ $item->kondisi }}</td>
+            <td>{{ $item->lokasi }}</td>
         </tr>
     @endforeach
 </table>
