d181a156bef8eac9ef13f42100c7c33d203d9caf
Daru Jinan Najmi <darujinan10@gmail.com>
2025-12-31 13:41:14 +0700

    tes push

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\Auth\RegisterDosenController;
 use App\Http\Controllers\Auth\RegisterMahasiswaController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SidebarController;
+use App\Http\Controllers\InventoryController;
+use App\Http\Controllers\User\PeminjamanUserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -12,9 +14,14 @@ Route::get('/', function () {
     return view('dashboard');
 })->name('dashboard');
 
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::redirect('/home', '/');
+
+// tak matiin dulu ya
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
@@ -47,3 +54,29 @@ Route::middleware(['auth', 'role:admin|super admin'])->group(function () {
 Route::post('/toggle-sidebar', [SidebarController::class, 'toggle'])
     ->middleware(['auth'])
     ->name('toggle-sidebar');
+
+// Inventory
+Route::get('/inventory', [InventoryController::class, 'index'])->name('inventory.index');
+Route::get('/inventory/create', [InventoryController::class, 'create'])->name('inventory.create');
+Route::post('/inventory', [InventoryController::class, 'store'])->name('inventory.store');
+Route::get('/inventory/{id}/edit', [InventoryController::class, 'edit'])->name('inventory.edit');
+Route::put('/inventory/{id}', [InventoryController::class, 'update'])->name('inventory.update');
+Route::delete('/inventory/{id}', [InventoryController::class, 'destroy'])->name('inventory.destroy');
+
+
+Route::middleware(['auth'])->prefix('borrowing')->group(function () {
+
+    // dashboard user peminjaman
+    Route::get('/dashboard', [PeminjamanUserController::class, 'dashboard'])
+        ->name('borrowing.dashboard');
+
+    // halaman pinjam barang
+    Route::get('/pinjam', [PeminjamanUserController::class, 'index'])
+        ->name('borrowing.pinjam');
+    Route::post('/pinjam', [PeminjamanUserController::class, 'store'])
+    ->name('borrowing.store');
+
+    // riwayat peminjaman
+    Route::get('/riwayat', [PeminjamanUserController::class, 'riwayat'])
+        ->name('borrowing.riwayat');
+});
